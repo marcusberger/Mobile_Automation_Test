8fb3e8713099798709de28840137f19f4f6b5c19
Marcus Berger <marcusvcberger@gmail.com>
2022-08-01 15:10:06 -0300

    adicionando classe runner

--- qaEngTest/src/main/java/runners/RunnerTest.java
+++ qaEngTest/src/main/java/runners/RunnerTest.java
@@ -1,4 +1,20 @@
 package runners;
 
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {"pretty"},
+        features = "src\\main\\resources\\features",
+        glue = "steps",
+        tags = {"@End2End"},
+        snippets = CucumberOptions.SnippetType.CAMELCASE,
+        monochrome = true,
+        dryRun = false
+)
 public class RunnerTest {
+
 }
+
